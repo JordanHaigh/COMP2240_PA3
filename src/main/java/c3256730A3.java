5f121fe59c521685e11a9cb821aecaf5dd3d35a6
Jordan Haigh <jhaigh1997@gmail.com>
2017-10-22 16:32:43 +1100

    Working on laptop. Added filereader class

--- src/main/java/c3256730A3.java
+++ src/main/java/c3256730A3.java
@@ -1,5 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,17 +15,15 @@ public class c3256730A3 implements ISubscriber
 
     private void run(String[]args) throws IOException {
         if(args.length == 0)
-        {
             throw new IllegalArgumentException("Error. Missing program arguments");
-        }
 
         List<Process> processList = new ArrayList<>();
+        ProcessFileReader processFileReader = new ProcessFileReader();
 
         for(String filePath: args)
         {
             //Read each process file
-            Process process = readProcess(filePath);
-
+            Process process = processFileReader.readProcess(filePath);
             processList.add(process);
         }
 
@@ -56,88 +52,7 @@ public class c3256730A3 implements ISubscriber
                 cpu.cycle();
             }
         }
-
-
     }
-    private Process readProcess(String filePath) throws IOException {
-        try(BufferedReader reader = new BufferedReader(new FileReader(filePath)))
-        {
-            String fileContents = readWholeFile(reader);
-
-            // Once file has been fully read, convert the string builder into a standard java string
-            return readProcessDataFromString(fileContents ,filePath);
-        }
-        catch(Exception e)
-        {
-            throw e;
-        }
-
-    }
-
-
-
-    /**
-     * public String readWholeFile(Buffered reader)
-     * Reads the entire input data file and appends to a stringbuilder
-     * @param reader - BufferedReader used to read the file
-     * @return - String containing all data
-     */
-    private String readWholeFile(BufferedReader reader) throws IOException
-    {
-        //Modified from http://abhinandanmk.blogspot.com.au/2012/05/java-how-to-read-complete-text-file.html
-        String line = null;
-        StringBuilder sb = new StringBuilder();
-        while((line = reader.readLine()) != null)
-        {
-            sb.append(line).append("\n");
-        }
-
-        return sb.toString();
-    }
-
-    private Process readProcessDataFromString(String fileContents, String filePath)
-    {
-        String processName = filePath.replaceAll(".txt","")
-                                    .replaceAll(".dat","")
-                                    .replaceAll("process","")
-                                    .replaceAll("Process","");
-
-        int processId = Integer.parseInt(processName);
-
-        //Cleanse line to remove \n, \t characters and make the any glyphs toUpperCase
-        String cleansedData = cleanseProcessData(fileContents);
-
-        String[] individualPages = cleansedData.split(" ");
-
-        List<Page> pageList = new ArrayList<>();
-
-        for(String page: individualPages)
-        {
-            int pageId = Integer.parseInt(page);
-            pageList.add(new Page(pageId));
-        }
-
-        Process process = new Process(processId, pageList);
-
-        return process;
-    }
-
-    private String cleanseProcessData(String fileContents)
-    {
-        String cleansed = fileContents.replaceAll("begin\n", "")
-                                    .replaceAll("end\n", "")
-                                    .replaceAll("\r\n", " ") // \r\n is windows version of new line
-                                    .replaceAll("\n", " ")
-                                    .replaceAll("\t", " ");
-
-
-
-        while (cleansed.contains("  "))
-            cleansed = cleansed.replace("  ", " ");
-
-        return cleansed;
-    }
-
 
     @Override
     public void handleMessage(ObservableMessage message) {
