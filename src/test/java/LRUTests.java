f231734c325f096d471d0d33675fbbcaf87f30fb
Jordan Haigh <jhaigh1997@gmail.com>
2017-10-23 17:04:38 +1100

    LRU working

--- src/test/java/LRUTests.java
+++ src/test/java/LRUTests.java
@@ -1,8 +1,6 @@
-import Algorithms.IPageReplacementAlgorithm;
-import Algorithms.LRU;
+import Algorithms.*;
 import Machine.Memory;
-import Model.Page;
-import Model.SchedulingProcess;
+import Model.*;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,15 +10,16 @@ import java.util.List;
 public class LRUTests {
     @Test
     public void getReplacementIndex() throws Exception
-    {
+    { PageGeneratorFactory factory = new PageGeneratorFactory();
+
         List<Page> pageList = new ArrayList<Page>();
         int arr[] = {2,3,2,1,5,2,4,5,3,2,5,2};
         for(int i = 0; i < arr.length;i++)
         {
-            Page page = new Page(arr[i]);
+            Page page = factory.getPage(1,arr[i]);
             pageList.add(page);
-
         }
+
         SchedulingProcess schedulingProcess = new SchedulingProcess(1, pageList);
 
         for(Page page: pageList)
@@ -31,18 +30,22 @@ public class LRUTests {
         memory.setPageReplacementAlgorithm(clock);
 
         int i = 0;
+
         for(Page page: pageList)
         {
-            memory.addToMemory(page, i);
+            if(!page.isLoadedInMemory())
+                memory.addToMemory(page,i);
+            else
+            {
+                page.setUseBit(true);
+                page.setTimeLastUsed(i);
+            }
             i++;
         }
 
         Assert.assertEquals(3, memory.getFrames()[0].getPageNumber());
-        Assert.assertEquals(2, memory.getFrames()[1].getPageNumber());
-        Assert.assertEquals(5, memory.getFrames()[2].getPageNumber());
-
-
-
+        Assert.assertEquals(5, memory.getFrames()[1].getPageNumber());
+        Assert.assertEquals(2, memory.getFrames()[2].getPageNumber());
     }
 
 }
